b6f017884c769baae9a04c93be430f24fc0dcd8d
ichadhr <ichadhr@gmail.com>
2018-01-18 14:36:49 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,187 @@
+import os
+import xlrd
+import re
+import yaml
+import io
+import sys
+
+
+def main() :
+    # load config
+    config = conf()
+
+    if config['stsb'] != False :
+        stsb()
+        sys.exit()
+    else :
+        normal()
+        sys.exit()
+
+
+def stsb() :
+    # load config
+    config = conf()
+
+    # file config value
+    if isNotBlank(config["file"]) :
+        File = config["file"]
+    else :
+        print("file can't blank")
+        sys.exit()
+
+    # regex po config
+    regex_po = "[^A-Za-z0-9]"
+
+    # delimiter config
+    Delim = ';'
+
+    # file config value
+    if isNotBlank(config["file"]) :
+        File = config["file"]
+    else :
+        print("file can't blank")
+        sys.exit()
+
+    # PO number config
+    # if isNotBlank(config["stsb_po_no"]["row"]) :
+    #     row = config["stsb_po_no"]["row"]
+
+    # if isNotBlank(config["stsb_po_no"]["column"]) :
+    #     column = config["stsb_po_no"]["column"]
+
+    book = xlrd.open_workbook(File)
+    sheet = book.sheet_by_index(0)
+
+    data = sheet.cell_value(rowx=config["stsb_po_no"]["row"], colx=config["stsb_po_no"]["column"])
+
+    DocN = DocNum(File, config["stsb_po_no"]["row"], config["stsb_po_no"]["column"])
+    # DocN = str(DocN).split('.')[0]
+    DocN = re.sub("[^0-9]", "", DocN)
+
+    print(DocN)
+
+
+def normal() :
+    # load config
+    config = conf()
+
+    # file config value
+    if isNotBlank(config["file"]) :
+        File = config["file"]
+    else :
+        print("file can't blank")
+        sys.exit()
+
+
+    # regex po config
+    regex_po = "[^A-Za-z0-9]"
+
+    # delimiter config
+    Delim = ';'
+
+    a = code_store(File, config['code_store']['row'], config['code_store']['column'])
+    b = po(File, config['po_no']['row'], config['po_no']['column'])
+    c = barcode(File, config['barcode']['row'], config['barcode']['column'])
+    d = qty(File, config['qty']['row'], config['qty']['column'])
+    e = modal_karton(File, config['modal_karton']['row'], config['modal_karton']['column'])
+
+    DocN = DocNum(File, config['dokumen_no']['row'], config['dokumen_no']['column'])
+
+    DocN = str(DocN).split('.')[0]
+    DocN = re.sub(regex_po, "_", DocN)
+
+    ResFile = DocN + '.csv'
+
+    if os.path.exists(ResFile) :
+        os.remove(ResFile)
+
+    # create and open .csv file
+    csv = open(ResFile, 'w+')
+
+    # write first header
+    csv.write(config['main']['code_store']+Delim+config['main']['po_no']+Delim+config['main']['barcode']+Delim+config['main']['qty']+Delim+config['main']['modal_karton'])
+    csv.write('\n')
+
+    for resA, resB, resC, resD, resE in zip(a, b, c, d, e) :
+        resA = str(resA).split('.')[0]
+        resB = str(resB).split('.')[0]
+        resB = re.sub(regex_po, "_", resB)
+        resC = str(resC).split('.')[0]
+        resD = str(resD).split('.')[0]
+        resE = str(resE).split('.')[0]
+        print(resA+Delim+resB+Delim+resC+Delim+resD+Delim+resE)
+        csv.write(resA+Delim+resB+Delim+resC+Delim+resD+Delim+resE)
+        csv.write("\n")
+
+    csv.close()
+
+
+def DocNum(FilePath, x, y) :
+    book = xlrd.open_workbook(FilePath)
+    sheet = book.sheet_by_index(0)
+
+    data = sheet.cell_value(rowx=x, colx=y)
+
+    return data
+
+
+def code_store(FilePath, x, y) :
+    book = xlrd.open_workbook(FilePath)
+    sheet = book.sheet_by_index(0)
+
+    data = sheet.col_values(y, start_rowx=x, end_rowx=None)
+
+    return data
+
+def po(FilePath, x, y) :
+    book = xlrd.open_workbook(FilePath)
+    sheet = book.sheet_by_index(0)
+
+    data = sheet.col_values(y, start_rowx=x, end_rowx=None)
+
+    return data
+
+def barcode(FilePath, x, y) :
+    book = xlrd.open_workbook(FilePath)
+    sheet = book.sheet_by_index(0)
+
+    data = sheet.col_values(y, start_rowx=x, end_rowx=None)
+
+    return data
+
+def qty(FilePath, x, y) :
+    book = xlrd.open_workbook(FilePath)
+    sheet = book.sheet_by_index(0)
+
+    data = sheet.col_values(y, start_rowx=x, end_rowx=None)
+
+    return data
+
+def modal_karton(FilePath, x, y) :
+    book = xlrd.open_workbook(FilePath)
+    sheet = book.sheet_by_index(0)
+
+    data = sheet.col_values(y, start_rowx=x, end_rowx=None)
+
+    return data
+
+def conf() :
+    if os.path.exists('config.yaml') :
+        stream = file('config.yaml', 'r')
+        config = yaml.load(stream)
+        return config
+
+    else :
+        conf = open('config.ini', 'w+')
+        conf.write(_config)
+        conf.close()
+
+def isNotBlank(myString):
+    if myString and myString.strip():
+        #myString is not None AND myString is not empty or blank
+        return True
+    #myString is None OR myString is empty or blank
+    return False
+
+if __name__== "__main__":
+  main()
